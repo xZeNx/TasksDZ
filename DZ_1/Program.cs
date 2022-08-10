483ec038cbc44cd4079b0c1c8bedecf4e6da6147
ZeN <zensmi@yandex.ru>
2022-08-10 20:53:57 +0300

    Ignore

---
+++ DZ_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введи число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+if (firstNumber >= 100 & firstNumber <= 999)
+{
+    int SecondDigit (int number)
+    {
+        int result = (firstNumber/10)%10;
+        return result;
+    }
+Console.WriteLine(SecondDigit(firstNumber));
+}
+else
+{
+    Console.WriteLine ("Число не трёхзначное:");
+}
