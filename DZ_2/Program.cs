622f6921cff6a14c428feb7f52f4fb1c7811b495
ZeN <zensmi@yandex.ru>
2022-08-10 20:59:15 +0300

    DZ

---
+++ DZ_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.Write("Введи число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+string NumberText = Convert.ToString(Number);
+if (NumberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра в числе > " + NumberText[2]);  
+}
+else
+{
+    Console.WriteLine("> Нет третьей цифры");
+}
