99bc5df2ebbbf4770f2811f76fdffa42e4c03584
ZeN <zensmi@yandex.ru>
2022-08-10 21:05:20 +0300

    DZ

---
+++ DZ_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели и проверяет,
+// является ли этот день выходным.
+
+Console.WriteLine("Введи цифру дня недели");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number == 6 || Number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else if (Number < 1 || Number > 7)
+{
+    Console.WriteLine("В неделе семь дней!");
+}
+else
+    Console.WriteLine("Это не выходной");
